4033d129fa24ce341069636a8647a8a1282dc434
Monica Alvarez <mmalvar13@gmail.com>
2016-08-08 22:21:12 -0600

    deleted previous work. added navbar and first row

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 		<!-- ADD OTHER CSS  FILES HERE -->
 		<!--Custom CSS Goes Here-->
-		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+<!--		<link rel="stylesheet" href="css/style.css" type="text/css"/>-->
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -36,10 +36,9 @@
 		<header>
 			<div class="container">
 				<div class="row">
-					<div class="col-md-3">
+					<div class="col-md-12">
 						<nav class="navbar navbar-default">
 							<div class="container-fluid">
-
 								<!-- Brand and toggle get grouped for better mobile display -->
 								<div class="navbar-header">
 									<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -51,38 +50,62 @@
 									<a class="navbar-brand" href="#">Brand</a>
 								</div>
 
-							</div>
+								<!-- Collect the nav links, forms, and other content for toggling -->
+								<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+									<ul class="nav navbar-nav">
+										<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+										<li><a href="#">Link</a></li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#">Action</a></li>
+												<li><a href="#">Another action</a></li>
+												<li><a href="#">Something else here</a></li>
+												<li role="separator" class="divider"></li>
+												<li><a href="#">Separated link</a></li>
+												<li role="separator" class="divider"></li>
+												<li><a href="#">One more separated link</a></li>
+											</ul>
+										</li>
+									</ul>
+									<form class="navbar-form navbar-left">
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Search">
+										</div>
+										<button type="submit" class="btn btn-default">Submit</button>
+									</form>
+									<ul class="nav navbar-nav navbar-right">
+										<li><a href="#">Link</a></li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#">Action</a></li>
+												<li><a href="#">Another action</a></li>
+												<li><a href="#">Something else here</a></li>
+												<li role="separator" class="divider"></li>
+												<li><a href="#">Separated link</a></li>
+											</ul>
+										</li>
+									</ul>
+								</div><!-- /.navbar-collapse -->
+							</div><!-- /.container-fluid -->
 						</nav>
 					</div>
-					<div class="col-md-5">
+				</div>
+			</div>
 
-					</div>
-					<div class="col-md-4">
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav navbar-right">
-								<li><a href="#">Link</a></li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-									<ul class="dropdown-menu">
-										<li><a href="#">Action</a></li>
-										<li><a href="#">Another action</a></li>
-										<li><a href="#">Something else here</a></li>
-										<li role="separator" class="divider"></li>
-										<li><a href="#">Separated link</a></li>
-									</ul>
-								</li>
-							</ul>
-						</div>
+			<div class="container">
+				<div class="row" style="height:20rem; outline:2px solid orange">
+					<div class="col-md-12">
+						<h2 class="text-center">Welcome Text</h2>
+						<p class="text-center">Byline Text</p>
 					</div>
 				</div>
 			</div>
 		</header>
 
-		<main>
-		</main>
+		<main></main>
 
-		<footer>
-		</footer>
+		<footer></footer>
 	</body>
 </html>
